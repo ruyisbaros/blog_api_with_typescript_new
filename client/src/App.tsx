df85001b0031ab5f785ce638112e0f826c193801
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-12-07 15:37:20 +0100

    client boiler plate set

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+export default App;
